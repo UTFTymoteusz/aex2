ff477887b44cce48528361eddb268bb68a0722b6
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-09-13 22:30:27 +0200

    threading tests

--- coremod/testmod/main.cpp
+++ coremod/testmod/main.cpp
@@ -9,12 +9,14 @@ const char* MODULE_NAME = "testmod";
 
 void test_mmap();
 void test_paging();
+void test_threads();
 
 void module_enter() {
     printk(PRINTK_WARN "testmod: Loaded\n");
 
     test_mmap();
     test_paging();
+    test_threads();
 }
 
 void module_exit() {
