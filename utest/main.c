3a594da81b13d06ebc14af917454ab54588587df
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-12-19 19:01:11 +0100

    subs

--- utest/main.c
+++ utest/main.c
@@ -6,11 +6,25 @@ int main(int argc, char* argv[]) {
     if (pid == 0)
         sleep(2);
 
+    char buffer[2048];
+    getcwd(buffer, sizeof(buffer));
+    printf("cwd: %s\n", buffer);
+
+    chdir("/bin/");
+
+    getcwd(buffer, sizeof(buffer));
+    printf("cwd: %s\n", buffer);
+
+    chdir("/dev");
+
+    getcwd(buffer, sizeof(buffer));
+    printf("cwd: %s\n", buffer);
+
     printf("argc: %i\n", argc);
     for (int i = 0; i < argc; i++)
         printf("%i: %s\n", i, argv[i]);
 
-    FILE* tty = fopen("/dev/tty0", "r+");
+    FILE* tty = fopen("tty0", "r+");
     fwrite("menel\n", 6, 1, tty);
 
     sleep(2);
